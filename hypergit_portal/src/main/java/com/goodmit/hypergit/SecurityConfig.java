46a5ac78287a25217280044485a457953e3d4986
drshin <dr.dice27@gmail.com>
2022-06-22 11:03:13 +0900

    hypergit portal 프로젝트 생성

---
+++ hypergit_portal/src/main/java/com/goodmit/hypergit/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.goodmit.hypergit;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Slf4j
+@EnableWebSecurity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/favicon.ico","/*.js");
+    }
+
+}
