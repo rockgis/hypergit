08ea316bdc98ef1eb41f8bc6aa2f1ecf60d7115f
laehoonlee <lhlee@goodmit.kr>
2022-07-19 00:04:57 +0900

    사용권한관리 수정

--- hypergit_portal/src/main/java/com/goodmit/hypergit/permissionmng/dto/Gittc0001Dto.java
+++ hypergit_portal/src/main/java/com/goodmit/hypergit/permissionmng/dto/Gittc0001Dto.java
@@ -24,8 +24,6 @@ public class Gittc0001Dto {
 
     private String roleId ;// ROLE_ID     VARCHAR(50) COMMENT '역할ID',
 
-    private String ugId ;//USR_NM      VARCHAR(30) COMMENT '사용자명',
-
     private String usrDcd; //  DESC_TT     VARCHAR(4000) COMMENT '설명',
 
 
@@ -36,9 +34,15 @@ public class Gittc0001Dto {
 
     private String emNm;  //EM_NM       CHAR(20) COMMENT '이메일',
 
+    private String useYn; // USE_YN  CHAR(1) COMMENT '사용유부 \',
+
     private String rgEn;// '등록사번',
 
     private String altEn;// '등록사번',
+
+    private String startDate;
+    private String endDate;
+
 
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
@@ -52,32 +56,36 @@ public class Gittc0001Dto {
                 .clNm(clNm)
                 .pgeUrlAr(pgeUrlAr)
                 .roleId(roleId)
-                .ugId(ugId)
+                .useYn(useYn)
                 .usrDcd(usrDcd)
                 .usrNm(usrNm)
                 .usrEn(usrEn)
                 .emNm(emNm)
                 .rgEn(rgEn)
                 .altEn(altEn)
+                .startDate(startDate)
+                .endDate(endDate)
                 .build();
         return gittc0001Entity;
     }
 
     @Builder
-    public Gittc0001Dto(Long id, int gsnId, String appNm, String clNm, String pgeUrlAr, String roleId, String ugId, String usrDcd, String usrNm, String usrEn, String emNm, String rgEn, String altEn, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public Gittc0001Dto(Long id, int gsnId, String appNm, String clNm, String pgeUrlAr, String roleId, String useYn, String usrDcd, String usrNm, String usrEn, String emNm, String rgEn, String altEn,String startDate,String endDate, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.gsnId = gsnId;
         this.appNm = appNm;
         this.clNm = clNm;
         this.pgeUrlAr = pgeUrlAr;
         this.roleId = roleId;
-        this.ugId = ugId;
+        this.useYn = useYn;
         this.usrDcd = usrDcd;
         this.usrNm = usrNm;
         this.usrEn = usrEn;
         this.emNm = emNm;
         this.rgEn = rgEn;
         this.altEn = altEn;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
